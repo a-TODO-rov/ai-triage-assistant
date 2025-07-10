78680d4aa47cbe5dabe4ff470a93998e3e401a44
aleksandar.todorov <a_t_todorov@yahoo.com>
2025-07-10 14:26:28 +0300

    Initial

---
+++ src/main/java/com/redis/aitriageassistant/AiTriageAssistantApplication.java
@@ -0,0 +1,13 @@
+package com.redis.aitriageassistant;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AiTriageAssistantApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AiTriageAssistantApplication.class, args);
+    }
+
+}
